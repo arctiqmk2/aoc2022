df1c2b5f3f1a3235a8850e6fa72a339f382bb7ce
Michael Kennedy <michael.kennedy@arctiq.ca>
2022-12-02 15:12:50 -0500

    cleaned up a little

--- dec02/src/main.rs
+++ dec02/src/main.rs
@@ -12,31 +12,15 @@ fn main() {
     let mut score: i32 = 0;
     let mut second_score: i32 = 0;
     for line in contents.split("\n") {
-        let mut line_score: i32 = 0;
-        let mut second_line_score: i32 = 0;
+        let mut line_score: i8 = 0;
+        let mut second_line_score: i8 = 0;
         if !line.is_empty() {
             let line_vec: Vec<char> = line.chars().collect();
             let first: char = line_vec[0];
             let last: char = line_vec[line_vec.len()-1];
-            let mut first_val: i32 = 0;
-            let mut last_val: i32 = 0;
-            let mut second_last_val: i32 = 0;
-            if first == 'A' {
-                first_val = 1;
-            } else if first == 'B' {
-                first_val = 2;
-            } else if first == 'C' {
-                first_val = 3;
-            }
-            if last == 'X' {
-                last_val = 1;
-            } else if last == 'Y' {
-                last_val = 2;
-            } else if last == 'Z' {
-                last_val = 3;
-            }
-            println!("{} {}", first, last);
-            println!("{} {}", first_val, last_val);
+            let second_last_val: i8;
+            let first_val:i8 = (first as i8)-64;
+            let last_val:i8 = (last as i8)-87;
             line_score += last_val;
             if (last_val - first_val) == 0 {
                 line_score += 3; 
@@ -48,45 +32,42 @@ fn main() {
                 line_score += 0;
             } else if (last_val - first_val) == -2 {
                 line_score += 6;
-            } else {
-                println!{"unexpected."}
             }
-            println!("{} line score", line_score);
+           //println!("{} line score", line_score);
             if last_val == 1 {
             // loss
-            if first_val == 1 {
-                second_last_val = 3;
-            } else if first_val == 2 {
-                second_last_val = 1;
-            } else {
-                second_last_val = 2;
-            }
-            println!("loss {} {} {}", first_val, last_val, second_last_val);
-            second_line_score += second_last_val;
+                if first_val == 1 {
+                    second_last_val = 3;
+                } else if first_val == 2 {
+                    second_last_val = 1;
+                } else {
+                    second_last_val = 2;
+                }
+                //println!("loss {} {} {}", first_val, last_val, second_last_val);
+                second_line_score += second_last_val;
             } else if last_val == 2 {
-            // draw
-            second_last_val = first_val;
-            println!("draw {} {} {}", first_val, last_val, second_last_val);
-            second_line_score += 3;
-            second_line_score += second_last_val;      
+                // draw
+                second_last_val = first_val;
+                //println!("draw {} {} {}", first_val, last_val, second_last_val);
+                second_line_score += 3;
+                second_line_score += second_last_val;      
             } else if last_val == 3 {
             //win
-            if first_val == 1 {
-                second_last_val = 2;
-            } else if first_val == 2 {
-                second_last_val = 3;
-            } else {
-                second_last_val = 1;
+                if first_val == 1 {
+                    second_last_val = 2;
+                } else if first_val == 2 {
+                    second_last_val = 3;
+                } else {
+                    second_last_val = 1;
+                }
+                //println!("win {} {} {}", first_val, last_val, second_last_val);
+                second_line_score += 6;
+                second_line_score += second_last_val;
             }
-            println!("win {} {} {}", first_val, last_val, second_last_val);
-            second_line_score += 6;
-            second_line_score += second_last_val;
-            }
-            println!("{} second line score", second_line_score);
-            } else {
+            //println!("{} second line score", second_line_score);
         }
-        score += line_score;
-        second_score += second_line_score;
+        score += line_score as i32;
+        second_score += second_line_score as i32;
     }
     println!("total score {}", score);
     println!("second score {}", second_score);
