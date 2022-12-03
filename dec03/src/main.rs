b47379251bae7198589d5518d09e84425a59bf06
Michael Kennedy <michael.kennedy@arctiq.ca>
2022-12-03 06:27:59 -0500

    small cleanup of dec03

--- dec03/src/main.rs
+++ dec03/src/main.rs
@@ -29,7 +29,6 @@ fn main() {
                     score += (((item as u8) - 38)) as u32;
                 }
             }
-            //println!("{:?}", frequency);        
         }
     }
     println!("score {}", score);
@@ -37,10 +36,8 @@ fn main() {
     let lines: Vec<&str> = contents.split("\n").collect();
     let mut groups: i32 = lines.len() as i32;
     groups = groups / 3;
-    //println!("there are {} groups", groups);
     for group in 0..groups {
         let index:usize = (group * 3).try_into().unwrap();
-        //println!("group {:?}", group);
         let mut pack1: Vec<char> = lines[index].chars().collect();
         let mut pack2: Vec<char> = lines[index+1].chars().collect();
         let mut pack3: Vec<char> = lines[index+2].chars().collect();
@@ -54,19 +51,19 @@ fn main() {
         packs.extend(pack2);
         packs.extend(pack3);
         let mut frequency: HashMap<&char, u32> = HashMap::new();
-            for char in &packs {
-                *frequency.entry(char).or_insert(0) += 1;
+        for char in &packs {
+            *frequency.entry(char).or_insert(0) += 1;
+        }
+        for (k, _v) in frequency.iter()
+                .filter(|&(_k, v)| (*v) == 3)
+                .map(|(k, v)| (k, v)) {
+            let item: char = **k;
+            if item.is_lowercase() {
+                score2 += ((item as u8) - 96) as u32;
+            } else {
+                score2 += (((item as u8) - 38)) as u32;
             }
-            for (k, _v) in frequency.iter()
-                    .filter(|&(_k, v)| (*v) == 3)
-                    .map(|(k, v)| (k, v)) {
-                let item: char = **k;
-                if item.is_lowercase() {
-                    score2 += ((item as u8) - 96) as u32;
-                } else {
-                    score2 += (((item as u8) - 38)) as u32;
-                }
-            }
+        }
     }
-    println!("score2 {}", score2);
+    println!("badge priority {}", score2);
 }
